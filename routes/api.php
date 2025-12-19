569427bd10b0028a1d8ef95bf22300602b691e91
muhamadpriskayamani <muhamadpriskayamani@smkwikrama.sch.id>
2025-12-19 10:45:04 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\SettingController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource(name: '/setting', controller: SettingController::class);
